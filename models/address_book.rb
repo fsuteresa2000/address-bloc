ab6fb3801adf973a7a6d6ac056bd24c34767a457
fsuteresa2000 <fsuteresa2000@yahoo.com>
2017-06-23 13:59:50 -0500

    assignment-7-searching-complete

--- models/address_book.rb
+++ models/address_book.rb
@@ -61,4 +61,14 @@ end
 
      return nil
    end
+
+   def iterative_search(name)
+     @entries.each do |entry|
+       if entry.name == name
+         return entry
+       end
+   end
+
+  return nil
+end
 end
